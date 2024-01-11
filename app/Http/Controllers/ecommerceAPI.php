34d01e3afce3c6124149ad06bb6328d8a9a0bb22
Eitanmohorade <eitanluc@gmail.com>
2024-01-11 19:07:57 -0300

    including laravel

---
+++ app/Http/Controllers/ecommerceAPI.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ecommerceAPI extends Controller
+{
+    //
+        function getData()
+        {
+            return ["name"=>"eiti"];
+        }
+}
